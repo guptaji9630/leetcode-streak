d304c075e91d37083d52048e777ef9fa15fd4111
Abhishek Gupta <75923437+guptaji9630@users.noreply.github.com>
2023-01-05 19:01:56 +0530

    452. Minimum Number of Arrows to Burst Balloons
    Runtime: 0 ms

---
+++ Day_10.cpp
@@ -0,0 +1,22 @@
+//452. Minimum Number of Arrows to Burst Balloons
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        if (points.size() == 0)
+            return 0;
+        sort(points.begin(), points.end(), [](vector<int> &a, vector<int> &b) {
+            return a[1] < b[1];
+        });
+        int count = 1;
+        int end = points[0][1];
+        for (int i = 1; i < points.size(); i++)
+        {
+            if (points[i][0] > end)
+            {
+                count++;
+                end = points[i][1];
+            }
+        }
+        return count;
+    }
+};
