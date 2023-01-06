bd94ff7114c684ed0216755e60bf62ba42911be8
Abhishek Gupta <75923437+guptaji9630@users.noreply.github.com>
2023-01-06 12:13:44 +0530

    1833. Maximum Ice Cream Bars
    Runtime: 2 ms

---
+++ Day_11.cpp
@@ -0,0 +1,21 @@
+// 1833. Maximum Ice Cream Bars
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+         sort(costs.begin(), costs.end());
+        int count = 0;
+        for (int i = 0; i < costs.size(); i++)
+        {
+            if (coins >= costs[i])
+            {
+                coins -= costs[i];
+                count++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return count;
+    }
+};
